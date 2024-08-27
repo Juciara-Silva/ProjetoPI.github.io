51750aa0fe8081857869e0b27e73198fdc71964c
esyhat <thayse.rn2018@gmail.com>
2024-08-27 10:35:19 -0300

    Commit

---
+++ ParteDeThayse/app/models/Visita.java
@@ -0,0 +1,18 @@
+package models;
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+
+public class Visita extends Model {
+	
+	public String nome;
+    public String email;
+    public String senha;  
+    public Date data;
+  
+ 
+}
